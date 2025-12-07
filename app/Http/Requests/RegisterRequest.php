82289ab41a293148a09c29cd4693ec02a814dc44
Zen <59063107+fauz2323@users.noreply.github.com>
2025-12-08 02:00:17 +0700

    fix: remove confirmation requirement for password in registration request

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -27,7 +27,7 @@ class RegisterRequest extends FormRequest
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
             'phone' => ['required', 'string', 'max:20', 'unique:' . User::class],
-            'password' => ['required', 'confirmed', Password::defaults()],
+            'password' => ['required', Password::defaults()],
         ];
     }
 
